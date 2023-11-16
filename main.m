911a73990c931478dd55cab483d92289b968df67
Kaan-Deveci <70139785+Kaan-Deveci@users.noreply.github.com>
2023-11-16 14:45:36 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,26 @@
+% Hypervolume-Based Evaluation and Ranking Technique (HEART) for Multi-Criteria Assessment
+% Developed by Kaan Deveci (devecik@itu.edu.tr)
+%
+% This script implements the HEART methodology as presented in the following publication:
+% Deveci, Kaan, and O. Guler. "Ranking Intuitionistic Fuzzy Sets with Hypervolume-Based Approach:
+% An Application for Multi-Criteria Assessment of Energy Alternatives." Applied Soft Computing (2023): 111038.
+%
+% If you utilize this code or adapt it for your research, please cite the above-mentioned article.
+%
+% Licensed under the MIT License. You may not use this file except in compliance with the License.
+% A copy of the License is available at https://opensource.org/licenses/MIT
+%
+% For the full license text, see the LICENSE file provided with this code.
+%
+% Disclaimer: This software is provided 'as is', without warranty of any kind, express or implied,
+% including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+% In no event shall the authors be liable for any claim, damages or other liability, whether in an action of contract,
+% tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
+%
+% Additional Note: While this code currently only includes the HEART methodology, it is designed to be
+% extensible. Researchers are welcome to extend this script by adding other methods at the end of the code as they see fit.
+% However, currently only the HyperVolume-based approach is implemented in this project.
+
 preparation1
 preparation2
 HVBasedMCDM
